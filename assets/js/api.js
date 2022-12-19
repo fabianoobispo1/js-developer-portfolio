8904ef766644d87f66fb08529fed4bb9551867a4
fabianoobispo1 <fabianoobispo@icloud.com>
2022-12-19 17:48:47 -0300

    edicao do json

--- assets/js/api.js
+++ assets/js/api.js
@@ -3,5 +3,6 @@ async function fetchProfileData() {
     const url = 'https://raw.githubusercontent.com/fabianoobispo1/js-developer-portfolio/main/data/profile.json';
     const response = await fetch(url)
     const profileData = await response.json()
+
     return profileData
 }
